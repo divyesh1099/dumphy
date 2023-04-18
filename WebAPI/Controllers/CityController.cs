802b8de6079359a15cb8ddbc3027c2182ce4ede9
Divyesh Vishwakarma <divyesh1099@gmail.com>
2023-04-18 17:17:17 +0530

    Add Create and Celete Async API for City

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAPI.Data;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -9,21 +11,39 @@ namespace WebAPI.Controllers
 	public class CityController : ControllerBase
 	{
 		private readonly DataContext _dataContext;
-		public CityController(DataContext dataContext) 
-			{ 
-				_dataContext = dataContext;
-			}
-		[HttpGet]
-		public IActionResult GetCities()
+		public CityController(DataContext dataContext)
 		{
-			var cities = _dataContext.Cities.ToList();
+			_dataContext = dataContext;
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetCities()
+		{
+			var cities = await _dataContext.Cities.ToListAsync();
 			return Ok(cities);
 		}
 
-		[HttpGet("{id}", Name ="Get")]
-		public string GetCity()
+		[HttpPost("addCity")]
+		public async Task<IActionResult> AddCity(City city)
 		{
-			return "aTLANTA";
+			City newCity = new City();
+			newCity = city;
+			await _dataContext.Cities.AddAsync(newCity);
+			await _dataContext.SaveChangesAsync();
+			return Ok(newCity);
+		}
+
+		[HttpPost("delete/{id}")]
+		public async Task<IActionResult> DeleteCity(int id)
+		{
+			City cityFromDb = await _dataContext.Cities.FindAsync(id);
+			if(cityFromDb != null)
+			{
+				var removedid = _dataContext.Cities.Remove(cityFromDb);
+				await _dataContext.SaveChangesAsync();
+				return Ok(removedid);
+
+			}
+			return NotFound();
 		}
 	}
 }
